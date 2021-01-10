f140eba81af43fc349fb3ba83565a8428e3b6e8f
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-10 10:41:37 -0800

    use moviecontroller class

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -15,6 +15,7 @@ class MoviesController extends Controller
     public function index()
     {
         $popularMovies = Http::get('https://api.themoviedb.org/3/movie/popular?api_key=8a9121945fb215b83aac6b1896a8adfe');
+        return $popularMovies;
         return view('dashboard', compact('popularMovies'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Facade\FlareClient\View;
 use Illuminate\Support\Facades\Route;
 
