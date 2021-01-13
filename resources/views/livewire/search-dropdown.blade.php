b76db36ee0658680edd79f1c2f3c82bb56d7db17
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 22:33:28 +0600

    show no result found badge

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -7,7 +7,7 @@
     </li>
     <div class="absolute bg-gray-800 rounded mt-2 w-64">
         <ul>
-            @if ($searchResult)
+            @if ($searchResult && $searchResult->cout() > 0)
 
             @foreach ($searchResult as $results)
 
@@ -17,6 +17,12 @@
                     class="block hover:bg-gray-700 px-3 py-2">{{ $results['title'] }}</a>
             </li>
             @endforeach
+            @else
+            <li class="border-b border-gray-700">
+                <a target="blank"
+                    href="javascript:avoid('0')"
+                    class="block hover:bg-gray-700 px-3 py-2 text-red-600 text-center">NO RESULTS FOUND</a>
+            </li>
             @endif
 
         </ul>
