749035d7cebe4c39459f580f1aa5a863358164f6
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 21:26:53 +0600

    remove dd

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -12,7 +12,7 @@ class SearchDropdown extends Component
     public function render()
     {
         $search = Http::get('https://api.themoviedb.org/3/search/movie?api_key=8a9121945fb215b83aac6b1896a8adfe&query=avengers')->json();
-        dd($search);
+
 
         return view('livewire.search-dropdown');
     }
