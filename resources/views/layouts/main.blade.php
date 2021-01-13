e292917d315bbcfb885dcfa519c972d86d4094bc
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 20:24:46 +0600

    cut seach option

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,11 +35,11 @@
                         <li class="md:ml-16 mt-3 md:mt-0 hover:text-gray-300"><a href="#">Actors</a></li>
                     </ul>
                     <ul class="flex flex-col md:flex-row items-center">
-                        <li class="reletive mt-2">
-                            <input type="text"
-                                class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-                                placeholder="Search...">
-                        </li>
+
+
+
+
+
                         <li class="mt-2">
                             <a href="">
                                 <img class="rounded-full ml-4"
