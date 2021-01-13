888a55cd1d7114f150c5185f3c67e3bf7f9fae94
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 20:25:10 +0600

    add  seach option

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- The whole world belongs to you --}}
+    <li class="reletive mt-2">
+        <input type="text"
+            class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
+            placeholder="Search...">
+    </li>
 </div>
