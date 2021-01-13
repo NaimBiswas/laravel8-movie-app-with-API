b4832740e9f174ec6d8f4638202455180c06a0d0
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 21:26:19 +0600

    add search api option

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 
 class SearchDropdown extends Component
@@ -10,6 +11,9 @@ class SearchDropdown extends Component
     public $search = '';
     public function render()
     {
+        $search = Http::get('https://api.themoviedb.org/3/search/movie?api_key=8a9121945fb215b83aac6b1896a8adfe&query=avengers')->json();
+        dd($search);
+
         return view('livewire.search-dropdown');
     }
 }
