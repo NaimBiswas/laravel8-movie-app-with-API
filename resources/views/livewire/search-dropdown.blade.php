c58ff781ffebe35edfeb4e646e7f309ec53e3c87
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-13 20:39:19 +0600

    search result showing design complete

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -4,4 +4,12 @@
             class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
             placeholder="Search...">
     </li>
+    <div class="absolute bg-gray-800 rounded mt-2 w-64">
+        <ul>
+            <li class="border-b border-gray-700">
+                <a href=""
+                    class="block hover:bg-gray-700 px-3 py-2">Avengers</a>
+            </li>
+        </ul>
+    </div>
 </div>
