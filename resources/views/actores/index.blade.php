5117c7fe101853ceb6f4bbdfc01cd91ca34ac43c
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-16 23:00:35 +0600

    after a long fun it showing the actores

--- resources/views/actores/index.blade.php
+++ resources/views/actores/index.blade.php
@@ -5,19 +5,18 @@
         <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold popular-movie">Popular Actors</h2>
         <div class="grid xs:grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12">
 
-            @foreach ($topRated as $tM)
-            @if ($loop->index <= 14)
-                <div
-                class="mt-8">
-                <a href="{{ route('movie.show', $tM['id']) }}">
-                    <img src="{{ "https://image.tmdb.org/t/p/w500/".$tM['poster_path'] }}"
+            @foreach ($popularActors as $pA)
+
+            <div class="mt-8">
+                <a href="{{ route('movie.show', $pA['id']) }}">
+                    <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $pA['profile_path'] }}"
                         alt="Move Poster"
                         class="hover:opacity-75 transition ease-in-out">
                 </a>
                 <div class="mt-2">
-                    <a href="{{ route('actor.show', $tM['id']) }}"
+                    <a href="{{ route('actor.show', $pA['id']) }}"
                         class="text-lg mt-2 hover:text-gray-300">
-                        {{ Str::limit($tM['title'], 22) }}
+                        {{ Str::limit($pA['name'], 22) }}
                     </a>
                     <div class="text-sm truncate text-gray-400">
                         <span>All Information here</span>
@@ -27,23 +26,20 @@
                         <span>All Information here</span>
                         <span>All Information here</span>
                     </div>
-
-
-                    @endforeach
                 </div>
+            </div>
+            @endforeach
+
+
+
         </div>
-        @endif
-        @endforeach
 
 
     </div>
+    <style>
+        .popular-movie {
+            color: #ed8936;
+        }
 
-
-</div>
-<style>
-    .popular-movie {
-        color: #ed8936;
-    }
-
-</style>
-@endsection
+    </style>
+    @endsection
