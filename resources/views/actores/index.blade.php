7881cd17228df75aa958649f7466de8b2c1f48bf
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-16 23:05:48 +0600

    change class for our actors images

--- resources/views/actores/index.blade.php
+++ resources/views/actores/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container mx-auto px-4 pt-16 border-b border-gray-800">
     <div class="popular-movies mb-12 ">
         <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold popular-movie">Popular Actors</h2>
-        <div class="grid xs:grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-12">
+        <div class="grid xs:grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
 
             @foreach ($popularActors as $pA)
 
