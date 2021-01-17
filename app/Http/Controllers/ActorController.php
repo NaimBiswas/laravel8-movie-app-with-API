cd8ae1c789bc7aa20f25f2658688ee56253f7210
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-17 12:47:53 +0600

    fix a issu

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Http;
 
 class ActorController extends Controller
 {
-    public function index($page)
+    public function index()
     {
 
         $popularActors = Http::get('https://api.themoviedb.org/3/person/popular?api_key=8a9121945fb215b83aac6b1896a8adfe')
